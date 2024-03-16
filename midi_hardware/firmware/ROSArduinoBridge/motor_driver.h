dcaf3c9fbd36f0c1b4e43d3b451f06590047a532
FurkannByrm <frknbyrm05@gmail.com>
2024-03-16 22:21:19 +0300

    diffbot hardware interface test

---
+++ midi_hardware/firmware/ROSArduinoBridge/motor_driver.h
@@ -0,0 +1,19 @@
+/***************************************************************
+   Motor driver function definitions - by James Nugen
+   *************************************************************/
+
+#ifdef L298_MOTOR_DRIVER
+  #define RIGHT_MOTOR_DIR  10
+  #define RIGHT_MOTOR_ENABLE 9
+  #define RIGHT_VREF 11
+  #define RIGHT_NSLEEP 12
+  #define LEFT_MOTOR_DIR 18
+  #define LEFT_MOTOR_ENABLE 13
+  #define LEFT_VREF 6
+  #define LEFT_NSLEEP 7
+
+#endif
+
+void initMotorController();
+void setMotorSpeed(int i, int spd);
+void setMotorSpeeds(int leftSpeed, int rightSpeed);
