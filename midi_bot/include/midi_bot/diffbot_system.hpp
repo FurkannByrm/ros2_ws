7b8712dc6ddccf53f5ae852f4a76a38555d8a1a6
FurkannByrm <frknbyrm05@gmail.com>
2024-08-11 22:40:24 +0300

    hardware interface(sunday)

--- midi_bot/include/midi_bot/diffbot_system.hpp
+++ midi_bot/include/midi_bot/diffbot_system.hpp
@@ -43,8 +43,7 @@ namespace midi_bot
         RCLCPP_SHARED_PTR_DEFINITIONS(MidiHardware);
 
         MIDI_BOT_PUBLIC
-        hardware_interface::CallbackReturn on_init(
-            const hardware_interface::HardwareInfo & info) override;
+        hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo& info) override;
         
         MIDI_BOT_PUBLIC
         std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
@@ -53,23 +52,23 @@ namespace midi_bot
         std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
         MIDI_BOT_PUBLIC
-        hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+        hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
 
         MIDI_BOT_PUBLIC
-        hardware_interface::CallbackReturn on_cleanup(const rclcpp_lifecycle::State & previous_state) override;
+        hardware_interface::CallbackReturn on_cleanup(const rclcpp_lifecycle::State& previous_state) override;
 
         MIDI_BOT_PUBLIC
-        hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+        hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
 
         MIDI_BOT_PUBLIC
-        hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+        hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
 
 
         MIDI_BOT_PUBLIC 
-        hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+        hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration& period) override;
 
         MIDI_BOT_PUBLIC
-        hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+        hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration& period) override;
         
         private:
 
