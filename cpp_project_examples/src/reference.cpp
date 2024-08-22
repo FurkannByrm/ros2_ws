7f7257864d5fb77ad5d7ac46b4e3a197a8be7e3b
FurkannByrm <frknbyrm05@gmail.com>
2024-08-22 17:19:34 +0300

    **

---
+++ cpp_project_examples/src/reference.cpp
@@ -0,0 +1,22 @@
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
+
+
+typedef geometry_msgs::msg::Pose pose;
+
+geometry_msgs::msg::Pose func(double &a, double &b, double &c, double &d){
+
+    pose msg;
+    msg.position.x = a;
+    
+
+
+}
+
+
+int main(int argc, char ** argv)
+{
+
+
+    return 0;
+}
