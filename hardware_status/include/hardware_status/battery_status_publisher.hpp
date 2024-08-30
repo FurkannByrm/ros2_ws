958f9deb340d02ca26d660f49db6b95b72017854
FurkannByrm <frknbyrm05@gmail.com>
2024-08-30 23:23:24 +0300

    hardware status projects

---
+++ hardware_status/include/hardware_status/battery_status_publisher.hpp
@@ -0,0 +1,9 @@
+#ifndef _BATTERY_STATUS_PUBLISHER_HPP_
+#define _BATTERY_STATUS_PUBLISHER_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "custom_interfaces/msg/bms_status.hpp"
+
+
+
+#endif //_BATTERY_STATUS_PUBLISHER_HPP_
---
+++ hardware_status/src/battery_status_publisher.cpp
@@ -0,0 +1,6 @@
+#include "hardware_status/battery_status_publisher.hpp"
+
+int main()
+{
+    return 0;
+}
