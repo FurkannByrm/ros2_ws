7e36ce7bc48e289cf2fc54dcd5a894e7cc62df1a
FurkannByrm <frknbyrm05@gmail.com>
2024-08-31 23:59:46 +0300

    status

--- hardware_status/include/hardware_status/battery_status_publisher.hpp
+++ hardware_status/include/hardware_status/battery_status_publisher.hpp
@@ -1,9 +1,23 @@
 #ifndef _BATTERY_STATUS_PUBLISHER_HPP_
 #define _BATTERY_STATUS_PUBLISHER_HPP_
 
+#include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "custom_interfaces/msg/bms_status.hpp"
 
+class BatteryStatus : public rclcpp::Node{
 
+    public:
+    BatteryStatus();
+
+    private:
+
+    void BatteryStatusCallBack();
+    rclcpp::Publisher<custom_interfaces::msg::BmsStatus>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+
+
+};
 
 #endif //_BATTERY_STATUS_PUBLISHER_HPP_
--- hardware_status/src/battery_status_publisher.cpp
+++ hardware_status/src/battery_status_publisher.cpp
@@ -1,6 +1,26 @@
 #include "hardware_status/battery_status_publisher.hpp"
 
-int main()
+
+BatteryStatus::BatteryStatus(): Node{"Bms_status_publisher_node"}
 {
-    return 0;
+    publisher_ = this->create_publisher<custom_interfaces::msg::BmsStatus>("bms_status",10);
+    timer_     = this->create_wall_timer(std::chrono::seconds(1),std::bind(&BatteryStatus::BatteryStatusCallBack,this));
+}
+
+
+void BatteryStatus::BatteryStatusCallBack()
+{
+    custom_interfaces::msg::BmsStatus msg;
+    msg.battery_voltage = 1.2;
+    publisher_->publish(msg);
+    // RCLCPP_INFO(this->get_logger(),"Robot voltage is :  %f",msg.battery_voltage);
+}
+
+
+int main(int argc, char ** args)
+{
+    rclcpp::init(argc, args);
+    rclcpp::spin(std::make_shared<BatteryStatus>());
+    rclcpp::shutdown();
+    
 }
